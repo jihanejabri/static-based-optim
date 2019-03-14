a8f797dd211ed92e202a17741162fb0cca0b0acb
jihanejabri <35765837+jihanejabri@users.noreply.github.com>
2019-03-14 18:11:03 +0100

    Add files via upload

--- StaticBasedOptim/src/main/scala/Static/Dct.scala
+++ StaticBasedOptim/src/main/scala/Static/Dct.scala
@@ -4,24 +4,27 @@ import org.apache.spark.SparkContext
 
 class Dct(sc : SparkContext, dir : String) {
   val lubmEncoded = "C:\\Users\\jmehd\\Desktop\\Projet Big data\\jeu d\\LUBMInstances\\lubm1Encoded.txt\\"
-  val lubmConcept = "C:\\Users\\jmehd\\Desktop\\Projet Big data\\jeu d\\univ-bench_conceptsURL2Id"
-  val lubmProperties = "C:\\Users\\jmehd\\Desktop\\Projet Big data\\jeu d\\univ-bench_propertiesURL2Id"
+  val lubmConcept = "C:\\Users\\jmehd\\Desktop\\Projet Big data\\jeu d\\univ-bench_conceptsURL2Id.txt"
+  val lubmProperties = "C:\\Users\\jmehd\\Desktop\\Projet Big data\\jeu d\\univ-bench_propertiesURL2Id.txt"
 
   val data = sc.textFile(dir +lubmEncoded).map(x=>x.split(""))
   val conceptId2URI = data.map(x=>(x(1), x(0), x(2)))
-  conceptId2URI.persist
-  conceptId2URI.count
-  println("Data = "+conceptId2URI.count)
-  println("Data = "+data)
-  /*val concepts = sc.textFile(dct + lubmConcept).map(x=>x.split(""))
-  val conceptId2URI = concepts.map(x=>(x(1), x(0), x(2)))
   conceptId2URI.persist()
-  conceptId2URI.count*/
+  println("Data = "+conceptId2URI.count)
 
- /* val properties = sc.textFile(dct+ lubmProperties).map(x=>x.split(" "))
+  val concepts = sc.textFile(dir + lubmConcept).map(x=>x.split(""))
+  val conceptId2URII = concepts.map(x=>(x(1), x(0), x(2)))
+  conceptId2URII.persist()
+  println("conceptId2URII = "+conceptId2URII.count)
+
+  val conceptsURII2Id = concepts.map(x=>(x(0),x(1)))
+  conceptsURII2Id.persist
+  println("conceptsURII2Id = "+conceptsURII2Id.count)
+
+ /* val properties = sc.textFile(dir+ lubmProperties).map(x=>x.split(" "))
   val propertiesId2URI = properties.map(x=>(x(1), x(0), x(2)))
   propertiesId2URI.persist()
-  propertiesId2URI.count*/
+  println("propertiesId2URI = "+propertiesId2URI.count)*/
   /*
   val  dct = "C:\\Users\\jmehd\\Desktop\\Projet Big data\\jeu d\\LUBMInstances\\"
 

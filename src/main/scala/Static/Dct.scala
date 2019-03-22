daedfa16101736a273b39c149b219f059bcc66d2
jihanejabri <jabrijihane17@gmail.com>
2019-03-22 23:07:13 +0100

    nb concepts&properties

--- src/main/scala/Static/Dct.scala
+++ src/main/scala/Static/Dct.scala
@@ -60,13 +60,6 @@ class Dct(sc : SparkContext, dir : String) extends  Serializable {
   println("trilpesDF println= "+trilpesDF.take(4).foreach(println))
 
   /***************************** Statics : nb Concepts*********/
- /* val model = ModelFactory.createDefaultModel()
-  def numberOfConcepts()= {
-    val rdfType = model.createProperty("1")
-    val obj = model.createResource(trilpesDF)
-    val iterator = model.listSubjectsWithProperty(rdfType,obj)
-    iterator.toList.size()
-  }*/
   val Work : Long = 13312
   val Faculty : Long = 13184
   val Person : Long = 12288
@@ -124,8 +117,132 @@ class Dct(sc : SparkContext, dir : String) extends  Serializable {
 
   //// Nb concepts :
 
+  ///Work
+  val WorkT = conceptId2URI.filter(x=> x._3.equals(Work)) //1
+  println("Concepts Work count = "+WorkT.count)
+  ///Faculty
+  val FacultyT = conceptId2URI.filter(x=> x._3.equals(Faculty)) //3
+  println("Concepts Faculty count = "+FacultyT.count)
+  ///Person
+  val PersonT = conceptId2URI.filter(x=> x._3.equals(Person)) //7
+  println("Concepts Person count = "+PersonT.count)
+  ///Student
+  val StudentT = conceptId2URI.filter(x=> x._3.equals(Student)) //0
+  println("Concepts Student count = "+StudentT.count)
+  ///Institute
+  val GraduateStudentT = conceptId2URI.filter(x=> x._3.equals(GraduateStudent)) //1889
+  println("Concepts GraduateStudent count = "+GraduateStudentT.count)
+  ///Employee
+  val EmployeeT = conceptId2URI.filter(x=> x._3.equals(Employee)) //0
+  println("Concepts Employee count = "+EmployeeT.count)
+  ///VisitingProfessor
+  val VisitingProfessorT = conceptId2URI.filter(x=> x._3.equals(VisitingProfessor)) //1
+  println("Concepts VisitingProfessor count = "+VisitingProfessorT.count)
+  ///Chair
+  val ChairT = conceptId2URI.filter(x=> x._3.equals(Chair)) //0
+  println("Concepts Chair count = "+ChairT.count)
+  ///Thing
+  val ThingT = conceptId2URI.filter(x=> x._3.equals(Thing)) //1
+  println("Concepts Thing count = "+ThingT.count)
+  ///AdministrativeStaff
+  val AdministrativeStaffT = conceptId2URI.filter(x=> x._3.equals(AdministrativeStaff)) //2
+  println("Concepts AdministrativeStaff count = "+AdministrativeStaffT.count)
+  ///Book
+  val BookT = conceptId2URI.filter(x=> x._3.equals(Book)) //0
+  println("Concepts Book count = "+BookT.count)
+  ///Specification
+  val SpecificationT = conceptId2URI.filter(x=> x._3.equals(Specification)) //0
+  println("Concepts Specification count = "+SpecificationT.count)
+  ///JournalArticle
+  val JournalArticleT = conceptId2URI.filter(x=> x._3.equals(JournalArticle)) //0
+  println("Concepts JournalArticle count = "+JournalArticleT.count)
+  ///Professor
+  val ProfessorT = conceptId2URI.filter(x=> x._3.equals(Professor)) //2
+  println("Concepts Professor count = "+ProfessorT.count)
+  ///FullProfessor
+  val FullProfessorT = conceptId2URI.filter(x=> x._3.equals(FullProfessor)) //125
+  println("Concepts FullProfessor count = "+FullProfessorT.count)
+  ///Department
+  val DepartmentT = conceptId2URI.filter(x=> x._3.equals(Department)) //15
+  println("Concepts Department count = "+DepartmentT.count)
+  ///Publication
+  val PublicationT = conceptId2URI.filter(x=> x._3.equals(Publication)) //6000
+  println("Concepts Publication count = "+PublicationT.count)
+  ///UndergraduateStudent
+  val UndergraduateStudentT = conceptId2URI.filter(x=> x._3.equals(UndergraduateStudent)) //5916
+  println("Concepts UndergraduateStudent count = "+UndergraduateStudentT.count)
+  ///AssociateProfessor
+  val AssociateProfessorT = conceptId2URI.filter(x=> x._3.equals(AssociateProfessor)) //181
+  println("Concepts AssociateProfessor count = "+AssociateProfessorT.count)
+  ///Article
+  val ArticleT = conceptId2URI.filter(x=> x._3.equals(Article)) //1
+  println("Concepts Article count = "+ArticleT.count)
+  ///TechnicalReport
+  val TechnicalReportT = conceptId2URI.filter(x=> x._3.equals(TechnicalReport)) //23
+  println("Concepts TechnicalReport count = "+TechnicalReportT.count)
+  ///ConferencePaper
+  val ConferencePaperT = conceptId2URI.filter(x=> x._3.equals(ConferencePaper)) //7
+  println("Concepts ConferencePaper count = "+ConferencePaperT.count)
+  ///Director
+  val DirectorT = conceptId2URI.filter(x=> x._3.equals(Director)) //0
+  println("Concepts Director count = "+DirectorT.count)
+  ///SystemsStaff
+  val SystemsStaffT = conceptId2URI.filter(x=> x._3.equals(SystemsStaff)) //0
+  println("Concepts SystemsStaff count = "+SystemsStaffT.count)
+  ///Program
+  val ProgramT = conceptId2URI.filter(x=> x._3.equals(Program)) //0
+  println("Concepts Program count = "+ProgramT.count)
+  ///TeachingAssistant
+  val TeachingAssistantT = conceptId2URI.filter(x=> x._3.equals(TeachingAssistant)) //408
+  println("Concepts TeachingAssistant count = "+TeachingAssistantT.count)
+  ///Software
+  val SoftwareT = conceptId2URI.filter(x=> x._3.equals(Software)) //0
+  println("Concepts Software count = "+SoftwareT.count)
+  ///ResearchGroup
+  val ResearchGroupT = conceptId2URI.filter(x=> x._3.equals(ResearchGroup)) //224
+  println("Concepts ResearchGroup count = "+ResearchGroupT.count)
+  ///UnofficialPublication
+  val UnofficialPublicationT = conceptId2URI.filter(x=> x._3.equals(UnofficialPublication)) //1
+  println("Concepts UnofficialPublication count = "+UnofficialPublicationT.count)
+  ///GraduateCourse
   val GraduateCourseT = conceptId2URI.filter(x=> x._3.equals(GraduateCourse)) // 13696 = 822
   println("Concepts GraduateCourse count = "+GraduateCourseT.count)
+  ///Organization
+  val OrganizationT = conceptId2URI.filter(x=> x._3.equals(Organization)) //7
+  println("Concepts Organization count = "+OrganizationT.count)
+  ///AssistantProfessor
+  val AssistantProfessorT = conceptId2URI.filter(x=> x._3.equals(AssistantProfessor)) //147
+  println("Concepts AssistantProfessor count = "+AssistantProfessorT.count)
+  ///Lecturer
+  val LecturerT = conceptId2URI.filter(x=> x._3.equals(Lecturer)) //94
+  println("Concepts Lecturer count = "+LecturerT.count)
+  ///College
+  val CollegeT = conceptId2URI.filter(x=> x._3.equals(College)) //5
+  println("Concepts College count = "+CollegeT.count)
+  ///ResearchAssistant
+  val ResearchAssistantT = conceptId2URI.filter(x=> x._3.equals(ResearchAssistant)) //547
+  println("Concepts ResearchAssistant count = "+ResearchAssistantT.count)
+  ///AssistantProfessor
+  val PostDocT = conceptId2URI.filter(x=> x._3.equals(PostDoc)) //0
+  println("Concepts PostDoc count = "+PostDocT.count)
+  ///Research
+  val ResearchT = conceptId2URI.filter(x=> x._3.equals(Research)) //1
+  println("Concepts Research count = "+ResearchT.count)
+  ///Dean
+  val DeanT = conceptId2URI.filter(x=> x._3.equals(Dean)) //1
+  println("Concepts Dean count = "+DeanT.count)
+  ///University
+  val UniversityT = conceptId2URI.filter(x=> x._3.equals(University)) //3510
+  println("Concepts University count = "+UniversityT.count)
+  ///Manual
+  val ManualT = conceptId2URI.filter(x=> x._3.equals(Manual)) //1
+  println("Concepts Manual count = "+ManualT.count)
+  ///Schedule
+  val ScheduleT = conceptId2URI.filter(x=> x._3.equals(Schedule)) //6
+  println("Concepts Schedule count = "+ScheduleT.count)
+  ///ClericalStaff
+  val ClericalStaffT = conceptId2URI.filter(x=> x._3.equals(ClericalStaff)) //1
+  println("Concepts ClericalStaff count = "+ClericalStaffT.count)
 
 
   /***************************** Statics : nb Properties********/
@@ -165,9 +282,108 @@ class Dct(sc : SparkContext, dir : String) extends  Serializable {
   val researchInterest : Long = 704
   val degreeFrom : Long = 784
 
-
-  val telephoneP = conceptId2URI.filter(x=> x._1.equals(telephone)) // 13696 = 822
-  println("Properties telephoneP count = "+telephoneP.count)
+  ///telephone
+  val telephoneP = conceptId2URI.filter(x=> x._2.equals(telephone)) // 8330
+  println("Properties telephone count = "+telephoneP.count)
+  ///title
+  val titleP = conceptId2URI.filter(x=> x._2.equals(title)) //0
+  println("Properties title count = "+titleP.count)
+  ///researchProject
+  val researchProjectP = conceptId2URI.filter(x=> x._2.equals(researchProject)) //0
+  println("Properties researchProject count = "+researchProjectP.count)
+  ///doctoralDegreeFrom
+  val doctoralDegreeFromP = conceptId2URI.filter(x=> x._2.equals(doctoralDegreeFrom)) //540
+  println("Properties doctoralDegreeFrom count = "+doctoralDegreeFromP.count)
+  ///mastersDegreeFrom
+  val mastersDegreeFromP = conceptId2URI.filter(x=> x._2.equals(mastersDegreeFrom)) //540
+  println("Properties mastersDegreeFrom count = "+mastersDegreeFromP.count)
+  ///advisor
+  val advisorP = conceptId2URI.filter(x=> x._2.equals(advisor)) //3101
+  println("Properties advisor count = "+advisorP.count)
+  ///topObjectProperty
+  val topObjectPropertyP = conceptId2URI.filter(x=> x._2.equals(topObjectProperty)) //0
+  println("Properties topObjectProperty count = "+topObjectPropertyP.count)
+  ///headOf
+  val headOfP = conceptId2URI.filter(x=> x._2.equals(headOf)) //15
+  println("Properties headOf count = "+headOfP.count)
+  ///undergraduateDegreeFrom
+  val undergraduateDegreeFromP = conceptId2URI.filter(x=> x._2.equals(undergraduateDegreeFrom)) //2414
+  println("Properties undergraduateDegreeFrom count = "+undergraduateDegreeFromP.count)
+  ///publicationResearch
+  val publicationResearchP = conceptId2URI.filter(x=> x._2.equals(publicationResearch)) //0
+  println("Properties publicationResearch count = "+publicationResearchP.count)
+  ///topDataProperty
+  val topDataPropertyP = conceptId2URI.filter(x=> x._2.equals(topDataProperty)) //0
+  println("Properties topDataProperty count = "+topDataPropertyP.count)
+  ///member
+  val memberP = conceptId2URI.filter(x=> x._2.equals(member)) //0
+  println("Properties member count = "+memberP.count)
+  ///listedCourse
+  val listedCourseP = conceptId2URI.filter(x=> x._2.equals(listedCourse)) //0
+  println("Properties listedCourse count = "+listedCourseP.count)
+  ///affiliateOf
+  val affiliateOfP = conceptId2URI.filter(x=> x._2.equals(affiliateOf)) //0
+  println("Properties affiliateOf count = "+affiliateOfP.count)
+  ///tenured
+  val tenuredP = conceptId2URI.filter(x=> x._2.equals(tenured)) //0
+  println("Properties tenured count = "+tenuredP.count)
+  ///teachingAssistantOf
+  val teachingAssistantOfP = conceptId2URI.filter(x=> x._2.equals(teachingAssistantOf)) //407
+  println("Properties teachingAssistantOf count = "+teachingAssistantOfP.count)
+  ///emailAddress
+  val typeP = conceptId2URI.filter(x=> x._2.equals(_type)) //20659
+  println("Properties type count = "+typeP.count)
+  ///softwareDocumentation
+  val softwareDocumentationP = conceptId2URI.filter(x=> x._2.equals(softwareDocumentation)) //0
+  println("Properties softwareDocumentation count = "+softwareDocumentationP.count)
+  ///subOrganizationOf
+  val subOrganizationOfP = conceptId2URI.filter(x=> x._2.equals(subOrganizationOf)) //239
+  println("Properties subOrganizationOf count = "+subOrganizationOfP.count)
+  ///publicationAuthor
+  val publicationAuthorP = conceptId2URI.filter(x=> x._2.equals(publicationAuthor)) //10634
+  println("Properties publicationAuthor count = "+publicationAuthorP.count)
+  ///takesCourse
+  val takesCourseP = conceptId2URI.filter(x=> x._2.equals(takesCourse)) //21489
+  println("Properties takesCourse count = "+takesCourseP.count)
+  ///hasAlumnus
+  val hasAlumnusP = conceptId2URI.filter(x=> x._2.equals(hasAlumnus)) //0
+  println("Properties hasAlumnus count = "+hasAlumnusP.count)
+  ///orgPublication
+  val orgPublicationP = conceptId2URI.filter(x=> x._2.equals(orgPublication)) //0
+  println("Properties orgPublication count = "+orgPublicationP.count)
+  ///publicationDate
+  val publicationDateP = conceptId2URI.filter(x=> x._2.equals(publicationDate)) //0
+  println("Properties publicationDate count = "+publicationDateP.count)
+  ///softwareVersion
+  val softwareVersionP = conceptId2URI.filter(x=> x._2.equals(softwareVersion)) //0
+  println("Properties softwareVersion count = "+softwareVersionP.count)
+  ///worksFor
+  val worksForP = conceptId2URI.filter(x=> x._2.equals(worksFor)) //540
+  println("Properties worksFor count = "+worksForP.count)
+  ///memberOf
+  val memberOfP = conceptId2URI.filter(x=> x._2.equals(memberOf)) //7790
+  println("Properties memberOf count = "+memberOfP.count)
+  ///teacherOf
+  val teacherOfP = conceptId2URI.filter(x=> x._2.equals(teacherOf)) //1627
+  println("Properties teacherOf count = "+teacherOfP.count)
+  ///affiliatedOrganizationOf
+  val affiliatedOrganizationOfP = conceptId2URI.filter(x=> x._2.equals(affiliatedOrganizationOf)) //0
+  println("Properties affiliatedOrganizationOf count = "+affiliatedOrganizationOfP.count)
+  ///name
+  val nameP = conceptId2URI.filter(x=> x._2.equals(name)) //15972
+  println("Properties name count = "+nameP.count)
+  ///officeNumber
+  val officeNumberP = conceptId2URI.filter(x=> x._2.equals(officeNumber)) //0
+  println("Properties officeNumber count = "+officeNumberP.count)
+  ///age
+  val ageP = conceptId2URI.filter(x=> x._2.equals(age)) //0
+  println("Properties age count = "+ageP.count)
+  ///researchInterest
+  val researchInterestP = conceptId2URI.filter(x=> x._2.equals(researchInterest)) //447
+  println("Properties researchInterest count = "+researchInterestP.count)
+  ///degreeFrom
+  val degreeFromP = conceptId2URI.filter(x=> x._2.equals(degreeFrom)) //0
+  println("Properties degreeFrom count = "+degreeFromP.count)
 
   /****************************** QUERY 1 *********************/
   /*
